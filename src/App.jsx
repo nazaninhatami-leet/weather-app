2051a704ad57064ed8e54fd2371fe7f2d302fce4
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-20 14:11:13 +0330

    enable

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import { Container } from './components/container';
 import './App.css';
 import axios from 'axios';
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const appContext = createContext();
 
 function App() {
   const [data, setData] = useState();
-  const [location, setLocation] = useState('tehran');
+  const [location, setLocation] = useState('');
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const fetchData = async () => {
     const response = await axios.get(url).then((response) => {
       setData(response.data);
-      console.log(data);
     });
   };
 
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const searchBoxChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const searchPressEnter = (e) => {
+    if (e.code === 'Enter') {
+      fetchData();
+    }
+  };
+
   return (
-    <appContext.Provider value={{ data, location, url, fetchData }}>
+    <appContext.Provider
+      value={{ data, location, url, searchBoxChange, searchPressEnter }}
+    >
       <Container />
     </appContext.Provider>
   );
