1bfa65ed47d8fe18966a520087d4f5fb67b8c372
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-20 12:41:24 +0330

    fetch data on search box change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { Container } from './components/container';
 import './App.css';
+import axios from 'axios';
+import { createContext, useState } from 'react';
+
+export const appContext = createContext();
 
 function App() {
-  return <Container />;
+  const [data, setData] = useState();
+  const [location, setLocation] = useState('tehran');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+
+  const fetchData = async () => {
+    const response = await axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(data);
+    });
+  };
+
+  return (
+    <appContext.Provider value={{ data, location, url, fetchData }}>
+      <Container />
+    </appContext.Provider>
+  );
 }
 
 export default App;
