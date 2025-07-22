f176692545e49bada29d8cd4bd6958849133fd3a
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-22 21:09:16 +0330

    made additional structure

--- src/components/additional.jsx
+++ src/components/additional.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import { useContext } from 'react';
+import { appContext } from '../App';
+
 export function Additional({}) {
-  return <section className="additional">
-        <article className="item humidity"></article>
-        <article className="item visibility"></article>
-        <article className="item clouds"></article>
-        <article className="item wind"></article>
-        <article className="item pressure"></article>
-        <article className="item sunrise-sunset"></article>
-        <article className="item ground-level"></article>
-        <article className="item sea-level"></article>
-      </section>;
+  const { data } = useContext(appContext);
+  console.log(data);
+  return (
+    <section className="additional">
+      <article className="item humidity">
+        humidity: {data?.main?.humidity}
+      </article>
+      <article className="item visibility">
+        humidity: {data?.main?.humidity}
+      </article>
+      <article className="item clouds"></article>
+      <article className="item wind"></article>
+      <article className="item pressure"></article>
+      <article className="item sunrise-sunset"></article>
+      <article className="item ground-level"></article>
+      <article className="item sea-level"></article>
+    </section>
+  );
 }
-  
