0bd4ba07a16fe7872ab3e6a4c58d11dbb1aa2d67
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-22 20:55:04 +0330

    made useFetchData hook

---
+++ src/components/useFetchData.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useFetchData = () => {
+  const [data, setData] = useState();
+  const [location, setLocation] = useState('new york');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+
+  const fetchData = async () => {
+    const response = await axios.get(url).then((response) => {
+      setData(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const searchBoxChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const searchPressEnter = (e) => {
+    if (e.code === 'Enter') {
+      fetchData();
+    }
+  };
+
+  return { data, location, searchBoxChange, searchPressEnter };
+};
+export default useFetchData;
