35f9f8475b85085ca21bc424bc02bfe264cd1e28
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-22 20:54:28 +0330

    made useFetchData hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,22 @@
 import { Container } from './components/container';
 import './App.css';
-import axios from 'axios';
-import { createContext, useEffect, useState } from 'react';
+import { createContext } from 'react';
+import useFetchData from './components/useFetchData';
 
 export const appContext = createContext();
 
 function App() {
-  const [data, setData] = useState();
-  const [location, setLocation] = useState('new york');
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
-
-  const fetchData = async () => {
-    const response = await axios.get(url).then((response) => {
-      setData(response.data);
-    });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const searchBoxChange = (e) => {
-    setLocation(e.target.value);
-  };
-
-  const searchPressEnter = (e) => {
-    if (e.code === 'Enter') {
-      fetchData();
-    }
-  };
+  const { data, location, searchBoxChange, searchPressEnter } = useFetchData();
 
   return (
-      <appContext.Provider
-        value={{ data, location, url, searchBoxChange, searchPressEnter }}
-      >
-        <Container />
-      </appContext.Provider>
+    <appContext.Provider
+      value={{ data, location, searchBoxChange, searchPressEnter }}
+    >
+      <Container />
+    </appContext.Provider>
   );
 }
 
 export default App;
+
+// made useFetchData
