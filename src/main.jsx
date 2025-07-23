0ceb8cd4392d54834376b2ed254c9e154238e2e8
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-23 17:21:13 +0330

    made responsive pt1

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+// import WeatherSearchBox from './components/test.js';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <App />
+  // <WeatherSearchBox />
   // </StrictMode>
 );
