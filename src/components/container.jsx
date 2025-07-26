b1365b6e6b5dd54309dbf1648e9a28695935dd48
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-26 07:14:04 +0330

    made the new structure

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -3,8 +3,14 @@ import { WeatherInfo } from './weatherInfo';
 export function Container({}) {
   return (
     <div className="container">
-      <SearchBox />
-      <WeatherInfo />
+      <div className="contain">
+        <aside className="pt1">
+          <SearchBox />
+        </aside>
+        <aside className="pt2">
+          {/* <WeatherInfo /> */}
+        </aside>
+      </div>
     </div>
   );
 }
