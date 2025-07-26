7ae754c141c34fb1f746afb1488048701a2b5387
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-26 14:55:19 +0330

    built pt1

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App.jsx';
-// import WeatherSearchBox from './components/test.js';
+import './app.css';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
